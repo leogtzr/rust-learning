327777af99dfc4adac20886935217adaa509aed2
lgutierrezramirez <lgutierrezramirez@switchfly.com>
2025-04-17 10:25:38 -0600

    add loop for users input / stop the game when they find the right nunber

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,21 +1,25 @@
-use std::io;
+use std::{cmp::Ordering, io};
+use rand::Rng;
 
 fn main() {
-    println!("Please input your guess.");
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    loop {
+        print!("Your guess: ");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    // Result<x, y> is an enumeration.
-    // Variant: each possible state is a variant.
-    // :: associated function.
-    // {} is a placeholder. 
-
-    println!("You guesses: {}", guess);
-
-    let x = 5;
-    let y = 10;
-    println!("x = {x} and y + 2 = {}", y + 2);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
+    }
 }
