8f01192ad6b5b5e45ea238835bcc969b910d6324
lgutierrezramirez <lgutierrezramirez@switchfly.com>
2025-04-28 14:32:32 -0600

    add project about method syntax and associated functions

--- method-syntax1/src/main.rs
+++ method-syntax1/src/main.rs
@@ -16,6 +16,21 @@ impl Rectangle {
     }
 }
 
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
@@ -30,4 +45,22 @@ fn main() {
     if rect1.width() {
         println!("The rectangle has a nonzero width; it is {}", rect1.width);
     }
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let sq = Rectangle::square(10);
 }
