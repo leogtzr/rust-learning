af052148703cd2cf2faf5a42972a1031df8cdbbb
lgutierrezramirez <lgutierrezramirez@switchfly.com>
2025-05-06 14:20:35 -0600

    add more code about bringing paths into scope

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -14,10 +14,53 @@ mod front_of_house {
     }
 }
 
+use crate::front_of_house::hosting;
+
 pub fn eat_at_restaurant() {
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+
+    // Change our mind about what brea we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
     // Absolute path:
-    crate::front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 
     // Relative path
     front_of_house::hosting::add_to_waitlist();
+    // meal.seasonal_fruit = String::from("blueberries");
+}
+
+fn deliver_order() {}
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    // If we make an enum public, all the variants will be public
+    // Por lo general, los enums tienen bastantes variantes o literales, sería la mar de castroso
+    // tener que anotar todas las variantes de manera individual.
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        // We go to the parent module...
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
 }
